1e10aba1ffdfd7b95bcea4441706580b57c8610a
Lavender <susie204@naver.com>
2023-07-25 21:55:31 +0900

    good

--- Emotion App/ViewController.swift
+++ Emotion App/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstView: UIView!
+    
+    @IBOutlet var countLabel: UILabel!
+        var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        updateLabel()
+        }
+    
+    func updateLabel() {
+        countLabel.text = "\(count)"
+    }
+        
+    
+    @IBAction func countButtonTapped(_ sender: UIButton) {
+        count += 1
+    }
+    
+    
 
 }
 
---
+++ Emotion App/scoreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  scoreViewController.swift
+//  Emotion App
+//
+//  Created by 이승현 on 2023/07/25.
+//
+
+import UIKit
+
+class scoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
